e440ecea81a455b38576f8e36ecb40e0ba1d808a
Ostap12 <ostapbor@gmail.com>
2017-04-26 10:50:51 +0300

    added dao

--- src/main/java/ua/galicia/flowershop/dao/OrderDAO.java
+++ src/main/java/ua/galicia/flowershop/dao/OrderDAO.java
@@ -6,16 +6,17 @@ import ua.galicia.flowershop.model.CartInfo;
 import ua.galicia.flowershop.model.OrderDetailInfo;
 import ua.galicia.flowershop.model.OrderInfo;
 import ua.galicia.flowershop.model.PaginationResult;
- 
+
+
 public interface OrderDAO {
- 
+
     public void saveOrder(CartInfo cartInfo);
- 
+
     public PaginationResult<OrderInfo> listOrderInfo(int page,
-            int maxResult, int maxNavigationPage);
-    
+                                                     int maxResult, int maxNavigationPage);
+
     public OrderInfo getOrderInfo(String orderId);
-    
+
     public List<OrderDetailInfo> listOrderDetailInfos(String orderId);
- 
+
 }
--- src/main/java/ua/galicia/flowershop/dao/ProductDAO.java
+++ src/main/java/ua/galicia/flowershop/dao/ProductDAO.java
@@ -3,22 +3,23 @@ package ua.galicia.flowershop.dao;
 import ua.galicia.flowershop.entity.Product;
 import ua.galicia.flowershop.model.PaginationResult;
 import ua.galicia.flowershop.model.ProductInfo;
- 
+
+
 public interface ProductDAO {
- 
-    
-    
-    public Product findFlower(String code);
-    
-    public ProductInfo findFlowerInfo(String code) ;
-  
-    
+
+
+
+    public Product findProduct(String code);
+
+    public ProductInfo findProductInfo(String code) ;
+
+
     public PaginationResult<ProductInfo> queryProducts(int page,
                                                        int maxResult, int maxNavigationPage  );
-    
+
     public PaginationResult<ProductInfo> queryProducts(int page, int maxResult,
                                                        int maxNavigationPage, String likeName);
- 
+
     public void save(ProductInfo productInfo);
-    
+
 }
